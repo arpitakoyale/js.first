8233db1304d9f9e4510dea1da9f8afb8d50848b1
arpitakoyale <koyalearpita@gmail.com>
2025-08-02 16:52:19 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,30 @@
+/*for(let a=0;a<=10;++a){
+console.log("arpita")
+}
+
+let b=1
+while(x<=5){
+    console.log(b)
+    b++
+}*/
+//1. Print numbers from 1 to 10
+//Write a program to print numbers from 1 to 10 using a loop.
+ for(let a=1; a<=10; ++a){
+console.log(a);
+}
+
+//2. Print even numbers between 1 and 20
+//Use a loop to print all even numbers from 1 to 20.
+for(let b=1; b<=20;++b){
+    if(b % 2==0){
+        console.log(b)
+}
+}
+//3. Sum of first N natural numbers
+//Ask the user to enter a number N. Use a loop to calculate the sum from 1 to N.
+
+let number=prompt("enter the number")
+for(x=1; x<=10; x++){
+    number*x;
+    console.log(x)
+}
