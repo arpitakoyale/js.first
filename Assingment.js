20cd2cafac8caa68b04b453ae09126c496e82b91
arpitakoyale <koyalearpita@gmail.com>
2025-08-07 18:03:40 +0530

    Add files via upload

---
+++ Assingment.js
@@ -0,0 +1,13 @@
+//1. Conditional Statement Questions
+//1. Check if a number is positive or negative.
+
+//2. Check if a number is even or odd.
+let a=prompt("enter the number")
+if sum=(a % 2==0){
+console.log("the num is even:"+sum)
+}
+else{
+    console.log("the anw is odd:" +sum)
+}
+
+//3. Check if a person is eligible to vote (age >= 18)
